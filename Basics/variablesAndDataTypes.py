b00364ac37fa9e9f361ebdf5f7f37c4fa057d78f
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-09-23 13:08:48 -0300

    feat(Basics): create file variablesAndDataTypes

---
+++ Basics/variablesAndDataTypes.py
@@ -0,0 +1,22 @@
+print('Hello Word')
+
+word = 'word'
+print(word)
+
+paragraph = """
+    This is a
+    paragraph
+"""
+
+print(paragraph)
+
+#comment 
+print("comment after statement") #comment after
+
+'''
+    comment multiline
+'''
+
+#multiple statements on single line 
+x = 'foo'; name = 'oseias'
+
